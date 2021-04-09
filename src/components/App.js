7b07f81f01e09999284625d22097da4c86c267ef
amdy diop <amdy@dofbi0.2>
2021-04-09 14:19:34 +0000

    fist commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Banner from "./banier";
+import Cart from "./Cart";
+
+function App() {
+ return (<div><Banner/> <Cart/></div>)
+}
+
+export default App;
